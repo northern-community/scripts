69bc557f278414ead0029158b4328c938b90b46a
KingricharDV#0439 <northerncommunity1@gmail.com>
2019-05-11 21:58:02 -0400

    Update multi_refresh.sh

--- multi_refresh.sh
+++ multi_refresh.sh
@@ -22,7 +22,7 @@ echo "Removing binaries..."
 cd /usr/local/bin
 rm -rf northernd northern-cli northern-tx
 echo "Downloading latest binaries"
-wget https://github.com/northern-community/Northern/releases/download/untagged-8d4d75f283535b10dcc5/northern2.6.0.tar.gz
+wget https://github.com/northern-community/scripts/raw/af03c29e1dce38ed3650790f256d2a8e2b6a3fa7/northern2.6.0.tar.gz
 tar -xzf northern2.6.0.tar.gz
 sudo chmod 755 -R  /usr/local/bin/northern*
 rm -rf northern-cli.tar.gz

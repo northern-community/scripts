be0e293454c3e897b4b4cc96432ebd79cb8d2e7f
KingricharDV#0439 <northerncommunity1@gmail.com>
2019-05-17 18:25:10 -0400

    Update refresh.sh
    Updated wallet 2.6.0

--- refresh.sh
+++ refresh.sh
@@ -4,9 +4,9 @@ cd ~
 cd /usr/local/bin
 ./northern-cli stop
 rm -rf northernd northern-cli northern-tx
-wget https://github.com/northern-community/Northern/releases/download/untagged-8d4d75f283535b10dcc5/northern2.6.0.tar.gz
+wget https://github.com/northern-community/Northern/releases/download/2.6.0/northern2.6.0.tar.gz
 tar -xzf northern2.6.0.tar.gz
-rm -rf https://github.com/northern-community/Northern/releases/download/untagged-8d4d75f283535b10dcc5/northern2.6.0.tar.gz
+rm -rf northern2.6.0.tar.gz
 ./northernd -daemon
 sleep 30
 ./northern-cli getinfo

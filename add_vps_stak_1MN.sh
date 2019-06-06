75714ee63b15c18a407eb166079928b20fdc63f4
KingricharDV#0439 <northerncommunity1@gmail.com>
2019-06-06 10:00:00 -0400

    Update add_vps_stak_1MN.sh
    updated wallet to 2.6.1

--- add_vps_stak_1MN.sh
+++ add_vps_stak_1MN.sh
@@ -2,9 +2,9 @@ cd ~
 cd /usr/local/bin
 ./northern-cli stop
 rm -rf northernd northern-cli northern-tx
-wget https://github.com/northern-community/Northern/releases/download/2.6.0/northern2.6.0.tar.gz
-tar -xzf northern2.6.0.tar.gz
-rm -rf northern-cli.tar.gz
+wget https://github.com/northern-community/Northern/releases/download/2.6.1/northern_linux.2_6_1.tar.gz
+tar -xzf northern_linux.2_6_1.tar.gz
+rm -rf northern_linux.2_6_1.tar.gz
 echo "staking=1" >> /home/northern/.northern/northern.conf
 ./northernd -daemon
 ./northern-cli -datadir=/home/northern/.northern getnewaddress

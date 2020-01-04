1d3ed648d4b82abe4d8e7c8f6ac6c0e6443abf53
KingricharDV#0439 <northerncommunity1@gmail.com>
2020-01-04 09:07:53 -0600

    Update refresh.sh
    updated for v3.0.0

--- refresh.sh
+++ refresh.sh
@@ -4,9 +4,9 @@ cd ~
 cd /usr/local/bin
 ./northern-cli stop
 rm -rf northernd northern-cli northern-tx
-wget https://github.com/northern-community/Northern/releases/download/2.6.2/northern_linux.2_6_2.tar.gz
-tar -xzf northern_linux.2_6_2.tar.gz
-rm -rf northern_linux.2_6_2.tar.gz
+wget https://github.com/northern-community/Northern/releases/download/3.0.0/northern_linux.3_0_0.tar.gz
+tar -xzf northern_linux.3_0_0.tar.gz
+rm -rf northern_linux.3_0_0.tar.gz
 ./northernd -daemon
 sleep 30
 ./northern-cli getinfo

af252fe734e22d4b081380d29472f285f3f650d0
KingricharDV#0439 <northerncommunity1@gmail.com>
2020-04-27 15:02:13 -1000

    3.3.1 updated

--- add_vps_stak_1MN.sh
+++ add_vps_stak_1MN.sh
@@ -2,11 +2,11 @@ cd ~
 cd /usr/local/bin
 ./northern-cli stop
 rm -rf northernd northern-cli northern-tx
-wget https://github.com/northern-community/Northern/releases/download/3.2.0/northern_linux.3_2_0.tar.gz
-tar -xzf northern_linux.3_2_0.tar.gz
-rm -rf northern_linux.3_2_0.tar.gz
+wget https://github.com/northern-community/Northern/releases/download/3.3.1/northern_linux.3_3_1.tar.gz
+tar -xzf northern_linux.3_3_1.tar.gz
+rm -rf northern_linux.3_3_1.tar.gz
 echo "staking=1" >> /home/northern/.northern/northern.conf
-./northernd -daemon
+./northernd -daemon -resync
 ./northern-cli -datadir=/home/northern/.northern getnewaddress
 ./northern-cli -datadir=/home/northern/.northern masternode debug
 echo -e ${RED}"This is your VPS Staking wallet address for mn 1!"${NC}

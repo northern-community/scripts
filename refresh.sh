281683400a4750c46f41302465864cf8b770377a
KingricharDV#0439 <northerncommunity1@gmail.com>
2020-04-27 14:42:35 -1000

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -7,6 +7,6 @@ rm -rf northernd northern-cli northern-tx
 wget https://github.com/northern-community/Northern/releases/download/3.2.1/northern_linux.3_3_1.tar.gz
 tar -xzf northern_linux.3_3_1.tar.gz
 rm -rf northern_linux.3_3_1.tar.gz
-./northernd -daemon
+./northernd -daemon -resync
 sleep 30
 ./northern-cli getinfo
